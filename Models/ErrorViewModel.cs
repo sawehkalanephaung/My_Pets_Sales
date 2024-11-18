54319abef119d9815896899d5947c086dcdfb56c
Saw <ehkalanephaung96@gmail.com>
2024-11-18 23:03:04 +0700

    init commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace My_Pets_Sales.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
